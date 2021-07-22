ce53ce763237b6933e2ec43b542c0a2dd991385a
Joe Stubbs <joestubbs5@gmail.com>
2021-07-22 17:09:39 -0500

    make sleep conditional to fail fast by default.

--- service_entry.sh
+++ service_entry.sh
@@ -3,4 +3,6 @@
 export PYTHONPATH=$PYTHONPATH:/home/tapis
 cd /home/tapis/service; /usr/local/bin/gunicorn -k $workerCls -w $processes --threads $threads -b :5000 api:app
 
-while true; do sleep 86400; done
+if [$DEBUG_SLEEP_LOOP == "sleep"]; then
+  while true; do sleep 86400; done
+fi
